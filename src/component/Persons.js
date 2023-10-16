a8c61db34fdc48c26c23cca190a467accba08b65
Viktor <DarmokhvalViktor@gmail.com>
2023-10-16 23:20:55 +0300

    with fetch

--- src/component/Persons.js
+++ src/component/Persons.js
@@ -2,7 +2,7 @@
 // https://rickandmortyapi.com/
 //     https://rickandmortyapi.com/api/character
 //         Створити 6 персонажів
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Person} from "./Person";
 
 const Persons = () => {
@@ -14,9 +14,19 @@ const Persons = () => {
         {id: 6, name: "Abadango Cluster Princess", status: "Alive", species: "Alien", gender: "Female", image: "https://rickandmortyapi.com/api/character/avatar/6.jpeg"},
         {id: 7, name: "Abradolf Lincler", status: "unknown", species: "Human", gender: "Male", image: "https://rickandmortyapi.com/api/character/avatar/7.jpeg"},
     ]
+
+    const [species, setSpecies] = useState([]);
+
+    useEffect(() => {
+        fetch("https://rickandmortyapi.com/api/character/[1, 2, 3, 4, 5, 6, 7, 9, 10]").then(value => value.json()).then(value => setSpecies(value))
+    }, [])
+    console.log(species)
+
+    //first iterates with hardcoded array, second, not commented - with fetch
     return (
         <div id={"allPersonsBlock"}>
-            {personArray.map(person => <Person person={person} key={person.id}/>)}
+            {/*{personArray.map(person => <Person person={person} key={person.id}/>)}*/}
+            {species.map(person => <Person person={person} key={person.id}/>)}
         </div>
     );
 };
