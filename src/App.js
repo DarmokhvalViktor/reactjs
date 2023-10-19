3795d4cbca361c295c906732241b4212284c5166
Viktor <DarmokhvalViktor@gmail.com>
2023-10-19 13:24:06 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
-import {Launches} from "./components/Launches";
 
 
 const App = () => {
     return (
         <div>
-            <Launches/>
+            App
         </div>
     );
 };
--- src/services/launchService.js
+++
@@ -1,9 +0,0 @@
-import {axiosService} from "./axiosService";
-import {urls} from "../constants/urls";
-
-const launchService = {
-    getAll:() => axiosService.get(urls.launches.base),
-    getByFlightNumber: (flightNumber) => axiosService.get(urls.launches.byFlightNumber(flightNumber))
-}
-
-export {launchService}
