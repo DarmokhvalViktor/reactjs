db3266d853d38da5c2371e56394577b3965dd688
Viktor <DarmokhvalViktor@gmail.com>
2023-11-23 14:15:50 +0200

    Lesson_10_Homework with class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-
-
+import {Posts} from "./components/Posts/Posts";
+import {Comments} from "./components/Comments/Comments";
+import {Cars} from "./components/Cars/Cars";
 
 const App = () => {
     return (
         <div>
-            App
+            <Posts/>
+            <br/>
+            <Comments/>
+            <br/>
+            <Cars/>
         </div>
     );
 };
